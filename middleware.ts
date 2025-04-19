0b59adcd703a2de77703e8d12c3a7ad9406bf588
Ayushmaan Satyam <ayushsatyam1909@gmail.com>
2025-04-19 17:58:01 +0530

    removed toute.ts again for webhook

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-// Define public routes using a matcher
-const isPublicRoute = createRouteMatcher([
-  '/api/webhooks/clerk',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return;
-  // otherwise, Clerk will protect the route
-});
-
+import { clerkMiddleware } from "@clerk/nextjs/server";
+ 
+export default clerkMiddleware();
+ 
 export const config = {
-  matcher: [
-    // Skip static/internal stuff
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
